7201d41af175bcb1fc6b278d62cbfa261e0636ce
Bbay1-aau <bayanbay13579@gmail.com>
2025-03-12 10:58:39 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,18 @@ document.addEventListener("DOMContentLoaded", () => {
 
     inputs.forEach(input => {
         input.addEventListener("change", () => {
-            alert(`You named this bird: ${input.value}`);
+            alert(`Du har navngivet fuglen: ${input.value}!`);
         });
     });
 });
+
+// Tilføj en hemmelig besked
+function showAlert() {
+    const messages = [
+        "Nymph parakitter elsker solsikkefrø!",
+        "Vidste du? De kan lære at fløjte melodier 🎶",
+        "De kan leve i over 20 år!",
+        "Nymph parakitter er de næstmest populære kælefugle i verden!"
+    ];
+    alert(messages[Math.floor(Math.random() * messages.length)]);
+}
